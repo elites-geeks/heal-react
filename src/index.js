d1fdf41e91560f6e2717574452cc19a8c86a5ba9
areenjaradat <areenjaradat96@gmail.com>
2021-07-07 17:51:54 +0300

    continue with structure

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
+
 // import reportWebVitals from './reportWebVitals';
 // import store from './store'
 // import { Provider } from 'react-redux'
