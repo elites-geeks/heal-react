032d280ff6eddba40eeb89d34d6db6b2daf8cd42
Omar Ramadan <ramomar1992@outlook.com>
2021-07-08 09:32:35 +0300

    new cahgnes

--- src/components/Route/Route.jsx
+++ src/components/Route/Route.jsx
@@ -1,37 +1,36 @@
 
-import {Switch, Route} from 'react-router-dom';
+import { Switch, Route } from 'react-router-dom';
 
 import VisitType from '../VisitType/VisitType';
 import SubscribeToInsuranceCompany from '../SubscribeToInsuranceCompany/SubscribeToInsuranceCompany';
 import MakeDoctorVisit from '../MakeDoctorVisit/MakeDoctorVisit';
 import DoctorVisitReceiption from '../DoctorVisitReceiption/DoctorVisitReceiption';
-import Diagnosis from '../Diagnosis/Diagnosis' 
+import Diagnosis from '../Diagnosis/Diagnosis'
 function ClientProfile() {
-    return (
-      <>
+  return (
+    <>
       <Switch>
-                <Route path="/makenewVisit">
-                   <VisitType/>
-                </Route>
-                <Route path="/Subscribe">
-                  <SubscribeToInsuranceCompany/>
-                </Route>
-                <Route path="/Doctor/visit">
-                   <MakeDoctorVisit/>
-                   </Route>
-                   <Route path="/newVisitDoctor">
-                   <DoctorVisitReceiption/>
-                   </Route>
-                   <Route path="/patient/history">
-                  
-                   </Route>
-                   <Route path="/Write/dignoses">
-                       <Diagnosis/>
-                   </Route>
-            </Switch>
-      </>
-      
-    );
-  }
-  
-  export default ClientProfile;
+        <Route path="/makenewVisit">
+          <VisitType />
+        </Route>
+        <Route path="/Subscribe">
+          <SubscribeToInsuranceCompany />
+        </Route>
+        <Route path="/Doctor/visit">
+          <MakeDoctorVisit />
+        </Route>
+        <Route path="/newVisitDoctor">
+          <DoctorVisitReceiption />
+        </Route>
+        <Route path="/patient/history">
+        </Route>
+        <Route path="/Write/dignoses">
+          <Diagnosis />
+        </Route>
+      </Switch>
+    </>
+
+  );
+}
+
+export default ClientProfile;
