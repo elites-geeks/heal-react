55902583f057609bdd85cc89c5c89b62a4ecbef2
Omar Ramadan <ramomar1992@outlook.com>
2021-07-11 12:57:19 +0300

    new changes

--- src/components/addNewPolicy/addNewPolicy.jsx
+++ src/components/addNewPolicy/addNewPolicy.jsx
@@ -1,37 +1,37 @@
-import { Button} from 'react-bootstrap';
+import { Button } from 'react-bootstrap';
 import Form from 'react-bootstrap/Form';
 const addNewPolicy = (props) => {
   return (
-    
-    <div>
-     <Form>
-  <Form.Group controlId="formBasicEmail">
-    <Form.Label>Offer Name</Form.Label>
-    <Form.Control type="text"  />
-  
-  </Form.Group>
-  <Form.Group controlId="formBasicEmail">
-    <Form.Label>Offer covrage</Form.Label>
-    <Form.Control type="text"  />
-    
-  </Form.Group>
-  <Form.Group controlId="formBasicEmail">
-    <Form.Label>Offer cost per month</Form.Label>
-    <Form.Control type="text"  />
-   
-  </Form.Group>
 
-  <Form.Group controlId="formBasicPassword">
-    <Form.Label>Offer Cost per Year</Form.Label>
-    <Form.Control type="text"  />
-  </Form.Group>
-  <Form.Group controlId="formBasicCheckbox">
-    
-  </Form.Group>
-  <Button variant="primary" type="submit">
-    Add new
-  </Button>
-</Form>
+    <div>
+      <Form>
+        <Form.Group controlId="formBasicEmail">
+          <Form.Label>Offer Name</Form.Label>
+          <Form.Control type="text" />
+
+        </Form.Group>
+        <Form.Group controlId="formBasicEmail">
+          <Form.Label>Offer covrage</Form.Label>
+          <Form.Control type="text" />
+
+        </Form.Group>
+        <Form.Group controlId="formBasicEmail">
+          <Form.Label>Offer cost per month</Form.Label>
+          <Form.Control type="text" />
+
+        </Form.Group>
+
+        <Form.Group controlId="formBasicPassword">
+          <Form.Label>Offer Cost per Year</Form.Label>
+          <Form.Control type="text" />
+        </Form.Group>
+        <Form.Group controlId="formBasicCheckbox">
+
+        </Form.Group>
+        <Button variant="primary" type="submit">
+          Add new
+        </Button>
+      </Form>
     </div>
   );
 };
