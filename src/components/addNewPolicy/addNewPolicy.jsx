a1a7165080529d729c8cbc840874dd5dc5753312
abu-al3ees <afafhware610@gmail.com>
2021-07-12 22:47:15 +0300

    style footer

--- src/components/addNewPolicy/addNewPolicy.jsx
+++ src/components/addNewPolicy/addNewPolicy.jsx
@@ -1,28 +1,30 @@
-import { Button } from 'react-bootstrap';
+import { Button,Table} from 'react-bootstrap';
 import Form from 'react-bootstrap/Form';
+
 const addNewPolicy = (props) => {
+
   return (
 
     <div>
       <Form>
         <Form.Group controlId="formBasicEmail">
-          <Form.Label>Offer Name</Form.Label>
+          <Form.Label style={{color:'white'}}>Offer Name</Form.Label>
           <Form.Control type="text" />
 
         </Form.Group>
         <Form.Group controlId="formBasicEmail">
-          <Form.Label>Offer covrage</Form.Label>
+          <Form.Label style={{color:'white'}}>Offer covrage</Form.Label>
           <Form.Control type="text" />
 
         </Form.Group>
         <Form.Group controlId="formBasicEmail">
-          <Form.Label>Offer cost per month</Form.Label>
+          <Form.Label style={{color:'white'}}>Offer cost per month</Form.Label>
           <Form.Control type="text" />
 
         </Form.Group>
 
         <Form.Group controlId="formBasicPassword">
-          <Form.Label>Offer Cost per Year</Form.Label>
+          <Form.Label style={{color:'white'}}>Offer Cost per Year</Form.Label>
           <Form.Control type="text" />
         </Form.Group>
         <Form.Group controlId="formBasicCheckbox">
@@ -32,6 +34,27 @@ const addNewPolicy = (props) => {
           Add new
         </Button>
       </Form>
+      <Table  style={{ width: "30rem", marginLeft: "10rem" }}
+          size="sm"
+          variant="dark"
+          responsive
+          striped
+          bordered
+          hover>
+  <thead>
+    <tr>
+      
+      <th>Offer Name</th>
+      <th>Offer covrage</th>
+      <th>Offer cost per month</th>
+      <th>Offer cost per Year</th>
+    </tr>
+  </thead>
+  <tbody>
+    
+  </tbody>
+</Table>
+      
     </div>
   );
 };
--- src/containers/Footer/Footer.jsx
+++ src/containers/Footer/Footer.jsx
@@ -1,9 +1,14 @@
 import React from 'react'
 import './Footer.scss'
-import logo from '../../components/Logo/logo.jpg'
+import logo from '../../components/Logo/logo.png'
+import FacebookIcon from '@material-ui/icons/Facebook';
+import GitHubIcon from '@material-ui/icons/GitHub';
+import TwitterIcon from '@material-ui/icons/Twitter';
+import LinkedInIcon from '@material-ui/icons/LinkedIn';
 function Footer() {
     return (
         <footer>
+            <div className='uls'>
             <ul>
                 <li>
                     Terms and Conditions
@@ -11,28 +16,43 @@ function Footer() {
                 <li>
                     Privacy
                 </li>
-                <li>
-                    Contact us
-                </li>
+               
             </ul>
-            <div className="copy">
-
-            <img alt="logo" src={logo} />
-            <p>
-                &copy;{new Date().getFullYear()} Heal Team
-            </p>
-            </div>
             <ul>
                 <li>
-                    About us
+                About us
                 </li>
+                <li>
+                Contact us <br />
+             
+                </li>
+                
+            </ul>
+           
+            <ul>
+               
                 <li>
                     Pricing
                 </li>
                 <li>
                     API
                 </li>
-            </ul>
+            </ul><br />
+            </div><br />
+            <div class="icons">
+        <FacebookIcon/>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+         <a href="https://github.com/elites-geeks/heal-react">  <GitHubIcon/>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;</a>
+                <LinkedInIcon/>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+                <TwitterIcon/>
+                
+            </div>
+            <div className="copy">
+
+<img alt="logo" src={logo} />
+<p>
+    &copy;{new Date().getFullYear()} Heal Team
+</p>
+</div>
         </footer>
     )
 }
