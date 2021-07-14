48c2b75f81bd50948c4e79d9b184c7ec50f87565
areenjaradat <areenjaradat96@gmail.com>
2021-07-14 09:34:07 +0300

    working in client side

--- src/components/AppointmentsPatient/AppointmentsPatient.jsx
+++ src/components/AppointmentsPatient/AppointmentsPatient.jsx
@@ -1,16 +1,21 @@
-import React, { useEffect } from "react";
+import React, { useEffect,useState } from "react";
 
-import { useState } from "react";
 import { useSelector } from "react-redux";
 import axios from "axios";
 import Card from "react-bootstrap/Card";
+import { If, Then, Else } from "react-if";
+import Loader from "../Loader/Loader";
+
 const baseURL = "https://elite-heal.herokuapp.com";
 
 const Appointments = () => {
+
   const [appointments, setappointments] = useState([]);
+  const [loading, setLoading] = useState(false);
+
   const userid = useSelector((state) => state.userReducer.user.parentId);
   useEffect(() => {
-
+    setLoading(true)
     fetchMyAPI();
     async function fetchMyAPI() {
       await axios({
@@ -26,29 +31,35 @@ const Appointments = () => {
       });
 
     }
-   
+    setLoading(false)
   }, [userid]);
   return (
-    <div>
+    <If condition={loading === true}>
+        <Then>
+          <Loader message="get the appointments..." />
+        </Then>
+        <Else><div>
       
-          {!appointments.length ? (
-            <h1>no available appointments</h1>
-          ) : (
-            appointments.map((appoint) => {
-              return (
-                <Card>
-                  <Card.Body>
-                  <Card.Header>Appointment</Card.Header>
-                    <p>date : {appoint.elem.date}</p>
-                    <p>time : {appoint.elem.time}</p>
-                    <p>name : {appoint.patient.userProfile.firstname} {appoint.patient.userProfile.lastname}</p>
-                  </Card.Body>
-                </Card>
-              );
-            })
-          )}
-       
-    </div>
+      {!appointments.length ? (
+        <h1>no available appointments</h1>
+      ) : (
+        appointments.map((appoint) => {
+          return (
+            <Card>
+              <Card.Body>
+              <Card.Header>Appointment</Card.Header>
+                <p>date : {appoint.elem.date}</p>
+                <p>time : {appoint.elem.time}</p>
+                <p>name : {appoint.patient.userProfile.firstname} {appoint.patient.userProfile.lastname}</p>
+              </Card.Body>
+            </Card>
+          );
+        })
+      )}
+   
+</div></Else>
+        </If>
+    
   );
 };
 
